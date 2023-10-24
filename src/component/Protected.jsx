defff6a08e122d2000157f0a6a5d44d598847e94
rayhanbabu <rayhanbabu458@gmail.com>
2023-10-24 10:05:54 +0600

    protected

--- src/component/Protected.jsx
+++ src/component/Protected.jsx
@@ -2,16 +2,14 @@ import {useNavigate} from "react-router-dom";
 import { useEffect } from "react";
 
 function Protected(props) {
-
     let Cmp=props.Cmp;
-
     let navigate = useNavigate();
 
-    useEffect(()=>{
-        if(!localStorage.getItem('loginInfo')){
-           navigate("/login")
-        }
-   },[])
+     useEffect(()=>{
+         if(!localStorage.getItem('loginInfo')){
+            navigate("/login")
+         }
+     },[])
     return (
         <div>
             <Cmp/>
